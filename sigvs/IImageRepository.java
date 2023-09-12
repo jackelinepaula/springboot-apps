bdb28e5f915b630392ca5d28c15f078d2f07ec8c
Jackeline Paula <56116537+jackelinepaula@users.noreply.github.com>
2023-09-12 10:51:29 -0300

    Delete sigvs/IImageRepository.java

--- sigvs/IImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.sigvs.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IImageRepository extends JpaRepository<Imagem, Long>{
-    
-}
